fbcb58ee776eb5fd67525574d345cbdaf73386d2
杨坤 <1351967859@qq.com>
2019-01-17 17:31:43 +0800

    第N次提交

--- app/src/main/java/zkch/com/exerdemo/cniaow/adapter/AppInfoAdapter.java
+++ app/src/main/java/zkch/com/exerdemo/cniaow/adapter/AppInfoAdapter.java
@@ -1,6 +1,8 @@
 package zkch.com.exerdemo.cniaow.adapter;
 
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.chad.library.adapter.base.BaseQuickAdapter;
@@ -24,7 +26,17 @@ public class AppInfoAdapter extends BaseQuickAdapter<AppInfo, BaseViewHolder> {
     @Override
     protected void convert(BaseViewHolder helper, AppInfo item) {
         Glide.with(getRecyclerView()).load(baseImgUrl + item.getIcon()).into((ImageView) helper.getView(R.id.img_app_icon));
-
+        helper.setText(R.id.txt_app_name, item.getDisplayName())
+                .setText(R.id.txt_brief, item.getBriefShow());
+        TextView txtViewPosition = helper.getView(R.id.txt_position);
+        txtViewPosition.setVisibility(mBuilder.isShowPosition ? View.VISIBLE : View.GONE);
+        txtViewPosition.setText(item.getPosition() + 1 + ". ");
+        TextView txtViewCategory = helper.getView(R.id.txt_category);
+        txtViewCategory.setVisibility(mBuilder.isShowCategoryName ? View.VISIBLE : View.GONE);
+        txtViewCategory.setText(item.getLevel1CategoryName());
+        TextView txtViewBrief = helper.getView(R.id.txt_brief);
+        txtViewBrief.setVisibility(mBuilder.isShowBrief ? View.VISIBLE : View.GONE);
+        txtViewBrief.setText(item.getBriefShow());
 
     }
 
